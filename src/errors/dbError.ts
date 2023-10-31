e6f1131cad3b86a5fde11c9545859f43e2aa570e
Kamil <kamgre7@gmail.com>
2023-10-31 18:44:21 +0100

    Added dbError

---
+++ src/errors/dbError.ts
@@ -0,0 +1,13 @@
+import { AppError } from './appError';
+
+export const DbErrorCodes = {
+  UNIQUE_CONSTRAINT_VIOLATION: '23505',
+  FOREIGN_KEY_VIOLATION: '23503',
+  NOT_NULL_VIOLATION: '23502',
+};
+
+export class DbError extends AppError {
+  constructor(message: string, code: number = 400) {
+    super(message, code);
+  }
+}
