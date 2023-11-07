1caba09367ffc3922cffa3e5a0a490a11f9a4ae4
Kamil <kamgre7@gmail.com>
2023-11-07 21:53:14 +0100

    Added actor routes

--- src/routes/mainRouter.ts
+++ src/routes/mainRouter.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import { userRouter } from '../domains/user/routes/userRouter';
 import { movieRouter } from '../domains/movie/routes/movieRouter';
+import { actorRouter } from '../domains/actor/routes/actorRoutes';
 
 export const mainRouter = Router();
 
 mainRouter.use('/user', userRouter);
 mainRouter.use('/movie', movieRouter);
+mainRouter.use('/actor', actorRouter);
