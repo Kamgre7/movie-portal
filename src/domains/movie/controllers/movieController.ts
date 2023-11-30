4c27e9479034e9e6824afa177f5647138fe4dd1d
Kamil <kamgre7@gmail.com>
2023-11-30 23:23:27 +0100

    Added new endpoint: rate, updateRate, addActorsToMovie

--- src/domains/movie/controllers/movieController.ts
+++ src/domains/movie/controllers/movieController.ts
@@ -6,11 +6,18 @@ import { FindMovieByIdReq } from '../schemas/findMovieByIdValidationSchema';
 import { TYPES } from '../../../ioc/types/types';
 import { IMovieService } from '../services/movieService';
 import { FindMovieByCriteriaReq } from '../schemas/findMovieByCriteriaValidationSchema';
+import { RateMovieReq } from '../schemas/rateMovieValidationSchema';
+import { AddActorMovieReq } from '../schemas/addActorsToMovieValidationSchema';
 
 export interface IMovieController {
   findById(req: ParsedRequest<FindMovieByIdReq>, res: Response): Promise<void>;
   findByCriteria(req: ParsedRequest<FindMovieByCriteriaReq>, res: Response): Promise<void>;
   create(req: ParsedRequest<CreateMovieReq>, res: Response): Promise<void>;
+
+  rate(req: ParsedRequest<RateMovieReq>, res: Response): Promise<void>;
+  updateRate(req: ParsedRequest<RateMovieReq>, res: Response): Promise<void>;
+
+  addActorsToMovie(req: ParsedRequest<AddActorMovieReq>, res: Response): Promise<void>;
 }
 
 @injectable()
@@ -20,30 +27,70 @@ export class MovieController implements IMovieController {
     private readonly movieService: IMovieService
   ) {}
 
-  findByCriteria = async (
-    req: ParsedRequest<FindMovieByCriteriaReq>,
-    res: Response
-  ): Promise<void> => {
-    const movies = await this.movieService.findByCriteria(req.query);
-
-    res.status(200).json({
-      movies,
-    });
-  };
-
-  findById = async (req: ParsedRequest<FindMovieByIdReq>, res: Response): Promise<void> => {
-    const movie = await this.movieService.findById(req.params.id);
-
-    res.status(200).json({
-      movie,
-    });
-  };
-
   create = async (req: ParsedRequest<CreateMovieReq>, res: Response): Promise<void> => {
     const movie = await this.movieService.create(req.body);
 
     res.status(201).json({
       movie,
     });
+  };
+
+  findById = async (req: ParsedRequest<FindMovieByIdReq>, res: Response): Promise<void> => {
+    const extensionCriteria = {
+      withRating: req.query.withRating === 'true',
+      withActors: req.query.withActors === 'true',
+    };
+
+    const movie = await this.movieService.findById(req.params.id, extensionCriteria);
+
+    res.status(200).json({
+      movie,
+    });
+  };
+
+  findByCriteria = async (
+    req: ParsedRequest<FindMovieByCriteriaReq>,
+    res: Response
+  ): Promise<void> => {
+    const extensionCriteria = {
+      withRating: req.query.withRating === 'true',
+      withActors: req.query.withActors === 'true',
+    };
+
+    const movies = await this.movieService.findByCriteria(req.query, extensionCriteria);
+
+    res.status(200).json({
+      movies,
+    });
+  };
+
+  rate = async (req: ParsedRequest<RateMovieReq>, res: Response): Promise<void> => {
+    const rating = {
+      movieId: req.params.id,
+      ...req.body,
+    };
+
+    const ratingInfo = await this.movieService.rate(rating);
+
+    res.status(201).json({
+      ratingInfo,
+    });
+  };
+
+  updateRate = async (req: ParsedRequest<RateMovieReq>, res: Response): Promise<void> => {
+    const rating = {
+      movieId: req.params.id,
+      ...req.body,
+    };
+
+    await this.movieService.updateRate(rating);
+
+    res.status(204).end();
+  };
+
+  addActorsToMovie = async (req: ParsedRequest<AddActorMovieReq>, res: Response) => {
+    await this.movieService.addActorsToMovie(req.params.id, req.body.actorIds);
+
+    res.status(204).end();
   };
 }
