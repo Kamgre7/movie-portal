f6e202f6133ab53d33b42fc62543d547c7b2751e
Kamil <kamgre7@gmail.com>
2023-12-12 21:42:04 +0100

    Added auth verify to routes

--- src/domains/auth/routes/authRoutes.ts
+++ src/domains/auth/routes/authRoutes.ts
@@ -4,17 +4,22 @@ import { TYPES } from '../../../ioc/types/types';
 import { IAuthController } from '../controllers/authController';
 import { requestValidator } from '../../../middlewares/requestValidator';
 import { LoginSchema } from '../schemas/loginValidationSchema';
-import { auth } from '../../../middlewares/auth';
 import { RefreshTokenSchema } from '../schemas/refreshTokenValidationSchema';
+import { IAuth } from '../../../middlewares/auth';
 
 export const authRouter = Router();
 
 const authController = container.get<IAuthController>(TYPES.AuthControllerToken);
+const authValidator = container.get<IAuth>(TYPES.AuthToken);
 
 authRouter.route('/login').post(requestValidator(LoginSchema), authController.login);
 
-authRouter.route('/logout').delete(auth, authController.logout);
+authRouter.route('/logout').delete(authValidator.verifyUser, authController.logout);
 
 authRouter
   .route('/token/refresh')
-  .post(requestValidator(RefreshTokenSchema), authController.refreshToken);
+  .post(
+    authValidator.verifyUser,
+    requestValidator(RefreshTokenSchema),
+    authController.refreshToken
+  );
