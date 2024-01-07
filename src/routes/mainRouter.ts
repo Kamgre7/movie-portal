93e71c7e6c43ca28d329ad6385550c7b58653eb5
Kamil <kamgre7@gmail.com>
2024-01-07 22:45:12 +0100

    Added swagger document loader and route to api-docs

--- src/routes/mainRouter.ts
+++ src/routes/mainRouter.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
+import { serve, setup } from 'swagger-ui-express';
 import { userRouter } from '../domains/user/routes/userRouter';
 import { movieRouter } from '../domains/movie/routes/movieRouter';
 import { actorRouter } from '../domains/actor/routes/actorRoutes';
 import { authRouter } from '../domains/auth/routes/authRoutes';
+import { swaggerDocument } from '../docs/swaggerLoader';
 
 export const mainRouter = Router();
 
@@ -10,3 +12,4 @@ mainRouter.use('/users', userRouter);
 mainRouter.use('/movies', movieRouter);
 mainRouter.use('/actors', actorRouter);
 mainRouter.use('/auth', authRouter);
+mainRouter.use('/api-docs', serve, setup(swaggerDocument));
